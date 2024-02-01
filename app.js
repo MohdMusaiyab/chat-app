2a308d30498e6a0214e52851eb0a72aa971dec15
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-02-01 16:06:21 +0530

    Total Clients Added

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server=app.listen(PORT, () => {
-  console.log("Server is running on port" + PORT);
+    console.log("Server is running on port" + PORT);
 });
 const io=require('socket.io')(server);
+let socketsConnected = new Set();
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on('connection',onConnected);
+function onConnected(socket){
+   console.log(socket.id)
+   socketsConnected.add(socket.id);
 
-io.on('connection',(socket)=>{
-    console.log(socket.id);
-})
+   io.emit('clients-total',socketsConnected.size);
+   socket.on('disconnect',()=>{
+    console.log(socket.id+" disconnected");
+    socketsConnected.delete(socket.id);
+    io.emit('clients-total',socketsConnected.size);
+   })
+}
--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
-const socket=io(
-    
-)
+const socket = io();
+
+const clientsTotal = document.getElementById("clients-total");
+// Listenign for an event from Backend
+socket.on("clients-total", (data) => {
+    // console.log(data);
+    clientsTotal.innerText='Ttoal clients connected: '+data;
+});
